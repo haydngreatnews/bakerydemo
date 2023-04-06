a9f800cf1028cd4b35e81cf905163fb957dc764d
Haydn <haydn+jacquard@newport.net.nz>
2023-04-06 20:52:24 +1200

    Add a gunicorn config

---
+++ gunicorn.py
@@ -0,0 +1,12 @@
+accesslog = "-"
+errorlog = "-"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" "%({X-Forwarded-For}i)s"'
+capture_output = True
+forwarded_allow_ips = "*"
+secure_scheme_headers = {"X-CLOUDFRONT": "yes"}
+workers = 2
+worker_class = "gevent"
+worker_connections = 5
+bind = ":8000"
+keep_alive = 75
+chdir = "/app"
